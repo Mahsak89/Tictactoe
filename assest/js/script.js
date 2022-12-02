f2c521b3476801b0dcf47560900de0e8d4a1ad00
Mahsak89 <khoshnoudmahsa@gmail.com>
2022-12-02 16:31:22 +0000

    Add code to elements

--- assest/js/script.js
+++ assest/js/script.js
@@ -5,3 +5,10 @@ let turn = PLAYER_X;
 
 const boardState = Array(tiles.length);
 boardState.fill(null);
+
+//Elements
+const strike = document.getElementById("strike");
+const gameOverArea = document.getElementById("game-over-area");
+const gameOverText = document.getElementById("game-over-text");
+const playAgain = document.getElementById("play-again");
+playAgain.addEventListener("click", startNewGame);
