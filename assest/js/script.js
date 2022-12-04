dbd2b56bd70bafb507c49f1b6e2da8a11ef780e9
Mahsak89 <khoshnoudmahsa@gmail.com>
2022-12-04 10:40:32 +0000

    Edit winni´ng combo in js

--- assest/js/script.js
+++ assest/js/script.js
@@ -120,14 +120,14 @@ function startNewGame() {
 
 const winningCombinations = [
   //rows
-  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
-  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
-  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
+  { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+  { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+  { combo: [7, 8, 9], strikeClass: "strike-row-3" },
   //columns
-  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
-  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
-  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+  { combo: [3, 6, 9], strikeClass: "strike-column-3" },
   //diagonals
-  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
-  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
+  { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+  { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
 ];
