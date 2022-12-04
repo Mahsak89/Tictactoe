54b34f9772a952b29f8295e80eca8b05e30ff528
Mahsak89 <khoshnoudmahsa@gmail.com>
2022-12-04 12:40:40 +0000

    Edit sounds

--- assest/js/script.js
+++ assest/js/script.js
@@ -17,7 +17,7 @@ playAgain.addEventListener("click", startNewGame)
 
 const gameOverSound = new Audio ("sounds/game_over.wav.mp3");
 const clickSound = new Audio ("sounds/click.wav.mp3");
-const drawOverSound = new Audio ("sounds/draw_over.wav.mp3");
+
 
 
 tiles.forEach((tile) => tile.addEventListener("click", tileClick));
@@ -43,91 +43,90 @@ function setHoverText() {
 
 
   
-function tileClick(event) {
+  function tileClick(event) {
     if (gameOverArea.classList.contains("visible")) {
       return;
     }
+  
     const tile = event.target;
     const tileNumber = tile.dataset.index;
     if (tile.innerText != "") {
       return;
     }
-
+  
     if (turn === PLAYER_X) {
-        tile.innerText = PLAYER_X;
-        boardState[tileNumber] = PLAYER_X;
-        turn = PLAYER_O;
-      } else {
-        tile.innerText = PLAYER_O;
-        boardState[tileNumber] = PLAYER_O;
-        turn = PLAYER_X;
-      }
-
-      clickSound.play();
-      setHoverText();
-      checkWinner();
-      
-}
-
-function checkWinner() {
-  //Check for a winner
-  for (const winningCombination of winningCombinations) {
-    //Object Destructuring
-    const { combo, strikeClass } = winningCombination;
-    const tileValue1 = boardState[combo[0]-1];
-    const tileValue2 = boardState[combo[1]-1];
-    const tileValue3 = boardState[combo[2]-1];
-
-    if (
-      tileValue1 != null &&
-      tileValue1 === tileValue2 &&
-      tileValue1 === tileValue3
-    ) {
-      strike.classList.add(strikeClass);
-      gameOverScreen(tileValue1);
-      return;
+      tile.innerText = PLAYER_X;
+      boardState[tileNumber - 1] = PLAYER_X;
+      turn = PLAYER_O;
+    } else {
+      tile.innerText = PLAYER_O;
+      boardState[tileNumber - 1] = PLAYER_O;
+      turn = PLAYER_X;
     }
+  
+    clickSound.play();
+    setHoverText();
+    checkWinner();
   }
-}
-
-
-  //Check for a draw
-  const allTileFilledIn = boardState.every((tile) => tile !== null);
-  if (allTileFilledIn) {
-    gameOverScreen(null);
- 
+  
+  function checkWinner() {
+    //Check for a winner
+    for (const winningCombination of winningCombinations) {
+      //Object Destructuring
+      const { combo, strikeClass } = winningCombination;
+      const tileValue1 = boardState[combo[0] - 1];
+      const tileValue2 = boardState[combo[1] - 1];
+      const tileValue3 = boardState[combo[2] - 1];
+  
+      if (
+        tileValue1 != null &&
+        tileValue1 === tileValue2 &&
+        tileValue1 === tileValue3
+      ) {
+        strike.classList.add(strikeClass);
+        gameOverScreen(tileValue1);
+        return;
+      }
+    }
+  
+    //Check for a draw
+    const allTileFilledIn = boardState.every((tile) => tile !== null);
+    if (allTileFilledIn) {
+      gameOverScreen(null);
+    }
+    
   }
-
-
-function gameOverScreen(winnerText) {
-  let text = "Draw!";
-  if (winnerText != null) {
-    text = `Winner is ${winnerText}!`;
+  
+  function gameOverScreen(winnerText) {
+    let text = "Draw!";
+    if (winnerText != null) {
+      text = `Winner is ${winnerText}!`;
+    }
+    gameOverArea.className = "visible";
+    gameOverText.innerText = text;
+    gameOverSound.play();
   }
-  gameOverArea.className = "visible";
-  gameOverText.innerText = text;
-  gameOverSound.play();
-}
-
-function startNewGame() {
-  strike.className = "strike";
-  gameOverArea.className = "hidden";
-  boardState.fill(null);
-  tiles.forEach((tile) => (tile.innerText = ""));
-  turn = PLAYER_X;
-  setHoverText();
-}
-
-const winningCombinations = [
-  //rows
-  { combo: [1, 2, 3], strikeClass: "strike-row-1" },
-  { combo: [4, 5, 6], strikeClass: "strike-row-2" },
-  { combo: [7, 8, 9], strikeClass: "strike-row-3" },
-  //columns
-  { combo: [1, 4, 7], strikeClass: "strike-column-1" },
-  { combo: [2, 5, 8], strikeClass: "strike-column-2" },
-  { combo: [3, 6, 9], strikeClass: "strike-column-3" },
-  //diagonals
-  { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
-  { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
-];
+  
+  function startNewGame() {
+    strike.className = "strike";
+    gameOverArea.className = "hidden";
+    boardState.fill(null);
+    tiles.forEach((tile) => (tile.innerText = ""));
+    turn = PLAYER_X;
+    setHoverText();
+  }
+  
+  
+  const winningCombinations = [
+    //rows
+    { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+    { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+    { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+    //columns
+    { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+    { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+    { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+    //diagonals
+    { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+    { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+  ];
